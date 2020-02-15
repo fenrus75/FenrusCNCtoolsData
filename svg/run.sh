01a85a1299d51fe8f5440e1585f891eab17a9d9e
Arjan van de Ven <arjanvandeven@gmail.com>
2020-02-15 08:28:43 -0800

    better makefile

--- svg/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-../../FenrusCNCtools/toolpath/toolpath --library ../../FenrusCNCtools/toolpath/toollib.csv --depth 0.125 --tool 301 --tool 102 --tool 18 --inlay --cutout 0.25  A3x3.svg 
-../../FenrusCNCtools/toolpath/toolpath --library ../../FenrusCNCtools/toolpath/toollib.csv --depth 0.125 --tool 301 --tool 102 --finish-pass testpattern.svg
-
